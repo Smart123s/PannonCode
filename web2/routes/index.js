7c6e3ca1fcaf04206d929befab0b30908dd81a21
Smart123s <public@tmbpeter.com>
2023-09-18 16:19:15 +0200

    web2: feladat 3ab

--- web2/routes/index.js
+++ web2/routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+var Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+const sequelize = new Sequelize('sqlite::%TEMP%/test.sqlite');
+const Color = sequelize.define('Color', {
+  color: DataTypes.STRING,
+});
+const User = sequelize.define('User', {
+  name: DataTypes.STRING,
+  age: DataTypes.NUMBER,
+  student: DataTypes.BOOLEAN,
+  working: DataTypes.BOOLEAN,
+  gender: DataTypes.BOOLEAN,
+  color: DataTypes.STRING,
+});
+
+// create tables
+Color.sync({ force: true });
+User.sync({ force: true });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  res.render('index', { colors: await Color.findAll() });
+});
+
+router.get('/add_color', async function(req, res, next) {
+  await Color.create({
+    color: req.query.color,
+  });
+  res.redirect("/");
+});
+
+router.get('/add_user', async function(req, res, next) {
+  await User.create({
+    name: req.query.name,
+    age: req.query.age,
+    student: req.query.student == 'on',
+    working: req.query.working == 'on',
+    gender: req.query.gender,
+    color: req.query.color,
+  });
+  res.redirect("/");
+});
+
+router.get('/get_users', async function(req, res, next) {
+  const users = await User.findAll();
+  res.send(JSON.stringify(users, null, 2));
 });
 
 module.exports = router;
