4b845fe7f636dec41c47aa4c36ecfce0a1568f3c
Smart123s <public@tmbpeter.com>
2024-03-15 12:48:56 +0100

    web2-2034: feladat2 (backend)

---
+++ web2-2024/feladat2/index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./chinook.db');
+
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const schemes = JSON.parse(fs.readFileSync('public/schemes.json', 'utf8'));
+
+app.post('/insert/:table', (req, res) => {
+    db.serialize(() => {
+        let table = req.params.table;
+        // fura sql injection ellenőrzés :)
+        if (schemes[table] === undefined) {
+            return res.status(400).send("Table not found");
+        }
+        let columns_num = schemes[table]["columns"].length;
+        let placeholders = Array(columns_num).fill('?').join(', ');
+        db.run(`INSERT INTO ${table} VALUES (${placeholders})`, req.body["values"], function (err) {
+            if (err) {
+                return res.status(400).send(err.message);
+            }
+            return res.sendStatus(200);
+        });
+    })
+})
+
+app.get('/select/:table', (req, res) => {
+    db.serialize(() => {
+        let table = req.params.table;
+        // fura sql injection ellenőrzés :)
+        if (schemes[table] === undefined) {
+            return res.status(400).send("Table not found");
+        }
+
+        db.all(`SELECT * FROM ${table}`, function (err, rows) {
+            if (err) {
+                return res.status(400).send(err.message);
+            }
+            return res.send(rows);
+        });
+    })
+})
+
+app.delete('/del/:table/', (req, res) => {
+    db.serialize(() => {
+        let table = req.params.table;
+        // fura sql injection ellenőrzés :)
+        if (schemes[table] === undefined) {
+            return res.status(400).send("Table not found");
+        }
+        let keys = schemes[table]["keys"];
+        let placeholders = keys.map((k) => `${k}=(?)`).join(' AND ');
+        db.run(`DELETE FROM ${table} WHERE ${placeholders}`, req.body["keys"], function (err) {
+            if (err) {
+                return res.status(400).send(err.message);
+            }
+            return res.sendStatus(200);
+        });
+    })
+})
+
+app.patch('/update/:table/', (req, res) => {
+    db.serialize(() => {
+        let table = req.params.table;
+        // fura sql injection ellenőrzés :)
+        if (schemes[table] === undefined) {
+            return res.status(400).send("Table not found");
+        }
+        let columns = schemes[table]["columns"];
+        let placeholders = columns.map((k) => `${k}=(?)`);
+        db.run(`UPDATE ${table}
+                SET ${placeholders.join(', ')}
+                WHERE ${placeholders.join(' AND ')}`, [...req.body["new"], ...req.body["original"]], function (err) {
+            if (err) {
+                return res.status(400).send(err.message);
+            }
+            return res.sendStatus(200);
+        });
+    })
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
+
+process.on('exit', function () {
+    db.close()
+});
