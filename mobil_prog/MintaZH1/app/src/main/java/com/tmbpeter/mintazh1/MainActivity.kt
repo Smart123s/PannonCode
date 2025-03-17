20b4364e10ebc043c8bfeb814ebac77975f0886e
Péter Tombor <public@tmbpeter.com>
2025-03-17 11:12:40 +0100

    mobilprog: mintazh1 (not finished)

---
+++ mobil_prog/MintaZH1/app/src/main/java/com/tmbpeter/mintazh1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.tmbpeter.mintazh1
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Toast.makeText(this, "Kezdődhet a játék",
+            Toast.LENGTH_LONG).show()
+
+        val i1 = findViewById<ImageView>(R.id.ImageView1)
+
+        i1.setOnClickListener {
+            i1.setImageResource(R.drawable.a)
+        }
+    }
+}
