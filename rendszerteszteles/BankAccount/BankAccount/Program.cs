a95465f26ff1f0c7afaab5999cc77a67cde04aa8
Péter Tombor <public@tmbpeter.com>
2025-11-19 10:59:56 +0100

    rendszerteszteles: upload sample project from Moodle
    https://moodle.uni-pannon.hu/pluginfile.php/545994/mod_resource/content/1/BankAccount.zip

---
+++ rendszerteszteles/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("John", 10000);
+            Console.WriteLine($"{bankAccount.CustomerName} - {bankAccount.Balance}");
+
+            bankAccount.Debit(2000);
+            Console.WriteLine($"{bankAccount.CustomerName} - {bankAccount.Balance}");
+
+            bankAccount.Credit(5000);
+            Console.WriteLine($"{bankAccount.CustomerName} - {bankAccount.Balance}");
+        }
+    }
+}
